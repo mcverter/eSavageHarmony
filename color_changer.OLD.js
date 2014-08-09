86734e6791137f7685a91c26fc7c12ded30ed852
Mitchell Verter <roadrunner@waste.org>
2014-08-09 10:19:18 -0500

    adding old color changer

---
+++ color_changer.OLD.js
@@ -0,0 +1,37 @@
+function getNextColor(colval) {
+    var rgb = /(\d+),\s*(\d+),\s*(\d+)/;
+    result = colval.match(rgb);
+    red = parseInt(result[1]);
+    green = parseInt(result[2]);
+    blue = parseInt(result[3]);
+    increment = 1;
+    nextred = (red + increment) % 256;
+    nextblue = (blue + increment) % 256;
+    nextgreen = (green + increment) % 256;
+
+    nextcolor = "rgb(" + nextred + ", " + nextgreen + ", " + nextblue + ")";
+    return nextcolor;
+}
+
+function changeColor()
+{
+    var allColorChangers = document.getElementsByClassName("color_changer")
+
+    for (var i=0; i<allColorChangers.length; i++) {
+        var elt = allColorChangers[i];
+
+        var cs = window.getComputedStyle(elt);
+        var clr = cs.color;
+        var bgcol = cs.backgroundColor;
+
+        console.log("elt " + elt);
+        console.log("colors " + clr + "  " + bgcol);
+        var nextcol = getNextColor(clr);
+        var nextbg = getNextColor(bgcol);
+        elt.style.color = nextcol;
+        elt.style.backgroundColor = nextbg;
+    }
+
+    t=setTimeout(function(){changeColor()},250);
+}
+
